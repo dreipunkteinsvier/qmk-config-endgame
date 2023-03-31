42c15285758e08b4885952f74b1bfedb6944943a
dreipunkteinsvier <dreipunkteinsvier@gmail.com>
2023-03-31 22:55:25 +0200

    added media controls to num layer

--- keymaps/pi/keymap.c
+++ keymaps/pi/keymap.c
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
    [_NUMBERS] = LAYOUT_eg(
-    KC_MINS,  KC_7,     KC_8,     KC_9,     KC_0,     _______,  _______,  _______,  _______,  _______,
+    KC_MINS,  KC_7,     KC_8,     KC_9,     KC_0,     _______,  KC_MPRV,  KC_VOLD,  KC_VOLU,  KC_MNXT,
     KC_EQL,   KC_4,     KC_5,     KC_6,     KC_QUOT,  KC_RALT,  KC_RSFT,  KC_LCTL,  KC_LALT,  KC_LGUI,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_BSLS,  _______,  _______,  _______,  _______,  _______,
     _______,                      _______,  _______,  _______,  _______,                      _______
